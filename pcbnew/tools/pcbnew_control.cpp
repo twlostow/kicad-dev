03b81d403015c7e62b527f84e2717c2d1b6206af
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2016-09-27 20:04:30 +0200

    Hide DLISTs behind iterators, first step towards refactoring the storage model

--- pcbnew/tools/pcbnew_control.cpp
+++ pcbnew/tools/pcbnew_control.cpp
@@ -807,7 +807,7 @@ int PCBNEW_CONTROL::AppendBoard( const TOOL_EVENT& aEvent )
 
     // Other items are appended to the item list, so keep trace to the last existing item is enough
     MODULE* module = board->m_Modules.GetLast();
-    BOARD_ITEM* drawing = board->m_Drawings.GetLast();
+    BOARD_ITEM* drawing = board->DrawingsList().GetLast();
     int zonescount = board->GetAreaCount();
 
     // Keep also the count of copper layers, to adjust if necessary
@@ -862,7 +862,7 @@ int PCBNEW_CONTROL::AppendBoard( const TOOL_EVENT& aEvent )
         m_toolMgr->RunAction( PCB_ACTIONS::selectItem, true, module );
     }
 
-    drawing = drawing ? drawing->Next() : board->m_Drawings;
+    drawing = drawing ? drawing->Next() : board->DrawingsList();
 
     for( ; drawing; drawing = drawing->Next() )
     {

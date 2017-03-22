a8b2fe6ec75b25c1fe040c2ea18df828757b5194
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2017-03-22 14:51:07 +0100

    Removed legacy connectivity/ratsnest algorithm, replaced with the new one. No legacy autorouting for the moment

--- include/wxBasePcbFrame.h
+++ include/wxBasePcbFrame.h
@@ -482,7 +482,7 @@ public:
      * It shows the connections from a pad to the nearest connected pad
      * @param aModule = module to consider.
      */
-    void build_ratsnest_module( MODULE* aModule );
+     void build_ratsnest_module( MODULE *mod, wxPoint aMoveVector );
 
     /**
      * Function TraceModuleRatsNest
@@ -527,7 +527,8 @@ public:
      * When aInit = false, aItemRef is not used (can be NULL)
      */
     void BuildAirWiresTargetsList( BOARD_CONNECTED_ITEM* aItemRef,
-                                   const wxPoint& aPosition, bool aInit );
+                                   const wxPoint& aPosition,
+                                    int aNet );
 
     /**
      * Function TestForActiveLinksInRatsnest

85ec008f39e20e0677fabf52404923a9b531128c
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2019-03-13 01:55:32 +0100

    Things starting to take shape...

--- qa/levenberg-marquardt/levenberg_marquardt.cpp
+++ qa/levenberg-marquardt/levenberg_marquardt.cpp
@@ -1,5 +1,5 @@
-#include <cstdio>
 #include <cmath>
+#include <cstdio>
 
 #include <levmar.h>
 
@@ -8,12 +8,11 @@
 //template<>
 void LM_SOLVER::init()
 {
-
 }
 
-void LM_SOLVER::evaluateFunc( double *params, double *x, int m, int n, void *adata)
+void LM_SOLVER::evaluateFunc( double* params, double* x, int m, int n, void* adata )
 {
-    auto self = reinterpret_cast<LM_SOLVER*> ( adata );
+    auto self = reinterpret_cast<LM_SOLVER*>( adata );
 
     int pindex = 0;
 
@@ -22,7 +21,7 @@ void LM_SOLVER::evaluateFunc( double *params, double *x, int m, int n, void *ada
         for( int i = 0; i < param->LmGetDimension(); i++ )
         {
             param->LmSetValue( i, params[pindex] );
-           // printf("Param %d: %.10f\n", pindex, param->LmGetValue( i ) );
+            // printf("Param %d: %.10f\n", pindex, param->LmGetValue( i ) );
             pindex++;
         }
     }
@@ -34,12 +33,12 @@ void LM_SOLVER::evaluateFunc( double *params, double *x, int m, int n, void *ada
     }
 }
 
-void LM_SOLVER::evaluateJacobian( double *params, double *jac, int m, int n, void *adata)
+void LM_SOLVER::evaluateJacobian( double* params, double* jac, int m, int n, void* adata )
 {
-    auto self = reinterpret_cast<LM_SOLVER*> ( adata );
+    auto self = reinterpret_cast<LM_SOLVER*>( adata );
 
-    for( int i = 0; i < m * n; i++ ) 
-        jac [ i ] = 0.0;
+    for( int i = 0; i < m * n; i++ )
+        jac[i] = 0.0;
 
     int pindex = 0;
 
@@ -48,7 +47,7 @@ void LM_SOLVER::evaluateJacobian( double *params, double *jac, int m, int n, voi
         for( int i = 0; i < param->LmGetDimension(); i++ )
         {
             param->LmSetValue( i, params[pindex] );
-          // printf("Param %d: %.10f\n", pindex, param->LmGetValue( i ) );
+            // printf("Param %d: %.10f\n", pindex, param->LmGetValue( i ) );
             pindex++;
         }
     }
@@ -56,101 +55,93 @@ void LM_SOLVER::evaluateJacobian( double *params, double *jac, int m, int n, voi
 
     for( auto eqn : self->m_equations )
     {
-        printf("** Do eqn %p [%d]\n", eqn, eqn->LmGetEquationCount() );
+        //printf( "** Do eqn %p [%d]\n", eqn, eqn->LmGetEquationCount() );
 
-        for(int p = 0; p < eqn->LmGetEquationCount(); p++)
+        for( int p = 0; p < eqn->LmGetEquationCount(); p++ )
         {
 
-            printf("Subeqn %d\n", p);
-            int jac_index = m * (eqn->m_eqnIndex+p) + eqn->m_eqnIndex;
-            eqn->LmDFunc( params + eqn->m_eqnIndex , &jac [ m * (eqn->m_eqnIndex+p) + eqn->m_eqnIndex ], p );
+            //printf( "Subeqn %d\n", p );
+            int jac_index = m * ( eqn->m_eqnIndex + p );
+            eqn->LmDFunc( &jac[m * ( eqn->m_eqnIndex + p )], p );
 
-            printf("jacobian: \n");
-            for( int j = 0; j < m; j++ ) 
+            //printf( "jacobian: \n" );
+            for( int j = 0; j < m; j++ )
             {
-                for( int i = 0; i < n; i++ ) 
+                for( int i = 0; i < n; i++ )
                 {
-                    printf( "%-10.3f", jac[ j * m + i ] );
+              //      printf( "%-10.3f", jac[j * m + i] );
                 }
-                printf("\n");
+               // printf( "\n" );
             }
         }
     }
 
-    for( int j = 0; j < m; j++ ) 
+    for( int j = 0; j < m; j++ )
     {
-        for( int i = 0; i < n; i++ ) 
+        for( int i = 0; i < n; i++ )
         {
-            printf( "%-10.3f", jac[ j * m + i ] );
+//            printf( "%-10.3f", jac[j * m + i] );
         }
-        printf("\n");
+  //      printf( "\n" );
     }
-        
-
 }
 
 bool LM_SOLVER::Solve()
 {
-    
-    m_workmem = new double [ LM_DER_WORKSZ(m_m, m_n) ];
 
-    double *p_vec = new double[ m_m ];
-    double *x_vec = new double[ m_n ];
-    int pindex = 0;
+    m_workmem = new double[LM_DER_WORKSZ( m_m, m_n )];
 
-    
+    double* p_vec = new double[m_m];
+    double* x_vec = new double[m_n];
+    int     pindex = 0;
+
+
     for( auto param : m_params )
     {
         for( int i = 0; i < param->LmGetDimension(); i++ )
         {
-            printf("Param %d: %.1f\n", pindex, param->LmGetValue( i ) );
+            //printf( "Param %d: %.1f\n", pindex, param->LmGetValue( i ) );
             p_vec[pindex++] = param->LmGetValue( i );
         }
     }
 
-    printf("Solve: params %d meas %d\n", m_m, m_n );
+    printf( "Solve: params %d meas %d\n", m_m, m_n );
 
-    for ( int i = 0; i < m_n; i++ )
+    for( int i = 0; i < m_n; i++ )
         x_vec[i] = 0.0;
 
-    auto rv = dlevmar_der(
-        evaluateFunc,
-        evaluateJacobian,
-        p_vec,
-        x_vec,
-        m_m,
-        m_n,
-        m_itmax,
-        nullptr,
-        nullptr,
-        m_workmem,
-        nullptr,
-        this );
+    auto rv = dlevmar_der( evaluateFunc, evaluateJacobian, p_vec, x_vec, m_m, m_n, m_itmax, nullptr,
+            nullptr, m_workmem, nullptr, this );
 
 
-    printf("rv %d\n", rv);
+    //printf( "rv %d\n", rv );
+
+
+    if( rv < 0 )
+    {
+        printf( "Solve failed.\n" );
+        rv = false;
+    }
+    else
+    {
+
+        pindex = 0;
+
+        for( auto param : m_params )
+        {
+            for( int i = 0; i < param->LmGetDimension(); i++ )
+            {
+                param->LmSetValue( i, p_vec[pindex] );
+                printf( "Param %d: %.10f %.10f\n", pindex, p_vec[pindex], param->LmGetValue( i ) );
+                pindex++;
+            }
+        }
+        rv = true;
+    }
 
     delete p_vec;
     delete x_vec;
     delete m_workmem;
 
-    if (rv < 0)
-    {
-        printf("Solve failed.\n");
-        return false;
-    }
-
-    pindex = 0;
-
-    for( auto param : m_params )
-    {
-        for( int i = 0; i < param->LmGetDimension(); i++ )
-        {
-            param->LmSetValue( i, p_vec[pindex] );
-            printf("Param %d: %.10f\n", pindex, param->LmGetValue( i ) );
-            pindex++;
-        }
-    }
-
-    return true;
+    return rv;
 }
